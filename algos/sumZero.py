80f13322ecdfb013bbb95f1c09ba943626ab0824
Spencer Bertsch <spencerbertsch99@gmail.com>
2021-01-15 18:16:08 -0800

    add sumZero solution

---
+++ algos/sumZero.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        """
+        Retruns a list of ints which sum to zero
+        """
+        # add the edge case where n=1
+        half_n: int = round((n * 0.5) - 0.1)  # <-- make sure we round down
+        if (n == 1):
+            sum_list: list = [0]
+
+        # if n is odd
+        elif (n%2 != 0):
+            sum_list: list = [0]
+            for i in range(half_n):
+                i_to_add = i+1
+                sum_list.append(-i_to_add)
+                sum_list.append(i_to_add)
+
+        # if n is even
+        elif (n%2 == 0):
+            sum_list: list = []
+            for i in range(half_n):
+                i_to_add = i+1
+                sum_list.append(-i_to_add)
+                sum_list.append(i_to_add)
+
+        return sum_list
