a058b1bdefad3ceb95ad206cef4d434ed6b0d168
Spencer Bertsch <spencerbertsch99@gmail.com>
2021-07-30 14:55:33 -0400

    Create 48_Rotate_Image.py

---
+++ algos/48_Rotate_Image.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        # we need to do a couple in-place transformations! 
+        
+        N: int = len(matrix)
+        
+        for i in range(N):
+            for j in range(i):
+                # we first need to do the transpose with a swap 
+                n1: int = matrix[i][j]
+                matrix[i][j] = matrix[j][i]
+                matrix[j][i] = n1
+                
+        for i in range(N):
+            for j in range(floor(N/2)):
+                # and now we need to swap the columns! 
+                n1: int = matrix[i][j]
+                matrix[i][j] = matrix[i][N-1-j]
+                matrix[i][N-1-j] = n1
+                
+        return matrix    
